b3f9906566d01685a4fd5722bab91358d18e557b
alireza <pourranjbar.ar@gmail.com>
2015-10-28 15:07:20 +0330

    added the stub classes related to B2C_Coupon and B2C2S Coupon

--- bitasync_site/models.py
+++ bitasync_site/models.py
@@ -7,3 +7,10 @@ class Data_Transfer_Plan(models.Model):
     plan_name = models.CharField(max_length=10)
     freq = models.IntegerField(default=0, null=True , blank=True)
     duration = DurationField(null=True , blank=True)
+
+class B2C_Coupon
+    pass
+
+
+class B2S2C_Coupon
+    pass
