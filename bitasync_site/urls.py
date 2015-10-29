49acbc19c6f59b427218b9bd4e8ecc2c87eed173
alireza <pourranjbar.ar@gmail.com>
2015-10-29 15:55:29 +0330

    changed the urls and views for data transfer plan page

--- bitasync_site/urls.py
+++ bitasync_site/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^data_transfer_plans$', views.data_transfer_plans, name='data_transfer_plans'),
+    url('^data_transfer_plans$', views.data_transfer_plans,
+        name='data_transfer_plans'),
 ]
--- bitasync_site/views.py
+++ bitasync_site/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Data_Transfer_Plans
+from .models import Data_Transfer_Plan
+from django.template import RequestContext, loader
+from django.http import Http404
+
 
 def homepage(request):
     return HttpResponse("This is the homepage")
 
+
 def data_transfer_plans(request):
     # return teh list of data_transfer_plans as a list.
+    plans = Data_Transfer_Plan.objects.all()
 
-    plans = Data_Transfer_Plans.objects.all()
-    
-    return HttpResponse("the list of data transfer plan")
+    if not plans:
+        raise Http404("No data transfer plans found")
+
+    template = loader.get_template('bitasync_site/data_transfer_plans.html')
+    context = RequestContext(request, {'plan_list': plans})
+    return HttpResponse(template.render(context))
