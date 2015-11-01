ef8a7b14bc4f34cdac4cd23c249da11c6cb8f3dd
alireza <pourranjbar.ar@gmail.com>
2015-11-01 17:17:37 +0330

    added some requried templates for authentication system: login.html, invalid.html, loggedin.html

--- bitasync/views.py
+++ bitasync/views.py
@@ -1,20 +1,42 @@
-from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 
+
 def login(request):
         c = {}
         c.update(csrf(request))
-        #return render_to_response('login.html', c)
-
         template = loader.get_template('user_profile/login.html')
         context = RequestContext(request, c)
         return HttpResponse(template.render(context))
- def auth_view(request):
-     username = request.POST.get('username','')
-     password = request.POST.get('password','')
 
-     
+
+def logout(request):
+    pass
+
+
+def auth_view(request):
+    # this view only performs an action based on the authentication form.
+    # therefore, it does not need a template
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/accounts/loggedin')
+    else:
+        return HttpResponseRedirect('/accounts/invalid')
+
+
+def loggedin(request):
+    template = loader.get_template('user_profile/loggedin.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
+
+def invalid(request):
+    pass
