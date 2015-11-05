99ea19c65d3dcf220b18e69609d88330b2dd7475
ha <bitapardazmobiledevelopment@gmail.com>
2015-11-05 15:59:31 +0330

    added static files in bitasync_site folder

---
+++ bitasync_site/static/bitasync_site/js/custom.js
@@ -0,0 +1,16 @@
+
+
+$(function() {
+   
+   $('.linkscroll').click(function()
+   { 
+	
+		
+		$('html, body').stop().animate({
+				scrollTop: $( $(this).attr('href') ).offset().top - 30
+			}, 800);
+			
+	});
+});
+
+	
