b31a8dbc4dd9f39d966bd5cb3b22bad791bad496
hamidk1010 <bitapardazmobiledevelopment@gmail.com>
2015-12-22 19:05:19 +0330

    interface change

---
+++ bitasync_site/static/bitasync_site/js/base.js
@@ -0,0 +1,15 @@
+$(function() {
+   
+	 $('#goport').click(function()
+   	{ 
+	
+		
+		 
+		 window.location = 'http://svn.bitasync.com/_Client/Client_Setup/BitaSyncInst.exe ';
+			
+	});
+	
+	
+});
+
+	
