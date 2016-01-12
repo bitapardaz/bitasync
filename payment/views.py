2e74ba6ea91b881021fca8598b426082edbc7f85
alireza <pourranjbar.ar@gmail.com>
2016-01-12 17:12:36 +0330

    second step payment

--- payment/views.py
+++ payment/views.py
@@ -36,6 +36,23 @@ def gateway(request):
                            SEND_URL_FINAL, PAYLINE_DOTIR_API_FINAL)
     return redirect(gateway_url)
 
+@csrf_exempt
+def result(request):
+
+    trans_id = request.POST['trans_id']
+    id_get = request.POST['id_get']
+
+    final_result = get_result(PAYLINE_DOTIR_API_FINAL, trans_id, id_get)
+
+    if int(final_result) == 1:
+        result_end = 'successful'
+    else:
+        result_end = 'UNsuccessful'
+
+    return render(request, 'successful_payment.html',
+                  {'result_end': result_end})
+
+
 @login_required
 def pay_for_a_plan(request,plan_name):
 
