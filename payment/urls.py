764789c6c6cc065f712cddafb3d495358f2f61b2
alireza <pourranjbar.ar@gmail.com>
2016-01-12 17:17:03 +0330

    second step payment url fixed

--- payment/urls.py
+++ payment/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^(?P<plan_name>\w\d)/$', views.pay_for_a_plan),
     url(r'^(?P<plan_name>\w\d)/(?P<token>[0-9A-Za-z]{1,20})/$', views.pay_for_a_plan_complete),
     url(r'^pay/$',views.pay),
-    url(r'^gateway/$',views.gateway)
+    url(r'^gateway/$',views.gateway),
+    url(r'^result/$',views.result),
 ]
