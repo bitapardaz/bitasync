90496db693c3c459ef5c9aff5583fcf6bb632ea8
alireza <pourranjbar.ar@gmail.com>
2016-01-16 11:52:04 +0330

    minor change in payment

--- payment/views.py
+++ payment/views.py
@@ -120,18 +120,20 @@ def result_payline(request,pending_purchase_hashcode):
     selected_plan = utility_functions.create_temp_plan(pending_purchase.data_transfer_plan, user_existing_coupons)
     context['selected_plan'] = selected_plan
 
-    response = None
+    return HttpResponse(final_result)
 
-    if int(final_result) == 1:
+#    response = None
+
+#    if int(final_result) == 1:
         # inset the purchase into database, and remove pending purchase
-        response =  pay_for_a_plan_success(request,pending_purchase,context,user_existing_coupons,selected_plan)
-    else:
-        response =  pay_for_a_plan_failure(request,context)
+#        response =  pay_for_a_plan_success(request,pending_purchase,context,user_existing_coupons,selected_plan)
+#    else:
+#        response =  pay_for_a_plan_failure(request,context)
 
     # remove pending purchase
-    pending_purchase.delete()
+#    pending_purchase.delete()
 
-    return response
+#    return response
 
 
 def pay_for_a_plan_success(request,pending_purchase,context,user_existing_coupons,selected_plan):
